d9bdf8ddd3ec9bb008894039a1f17361fe4e3e77
Postmodern <postmodern.mod3@gmail.com>
2008-01-27 01:17:36 +0000

    * Renamed Program.create_from_path to Program.find_with_path. * Renamed Program.create_from_paths to Program.find_with_paths. * Renamed Program.create to Program.find.

--- trunk/lib/rprogram/program.rb
+++ trunk/lib/rprogram/program.rb
@@ -41,11 +41,11 @@ module RProgram
     # is a valid file. Any given _args_ or a given _block_ will be used
     # in creating the new program.
     #
-    #   Program.create_from_path('/bin/cd') # => Program
+    #   Program.find_with_path('/bin/cd') # => Program
     #
-    #   Program.create_from_path('/obviously/fake') # => nil
+    #   Program.find_with_path('/obviously/fake') # => nil
     #
-    def self.create_from_path(path,*args,&block)
+    def self.find_with_path(path,*args,&block)
       return self.new(path,*args,&block) if File.file?(path)
     end
 
@@ -54,11 +54,11 @@ module RProgram
     # if a path within _paths_ is a valid file. Any given _args_ or
     # a given _block_ will be used in creating the new program.
     #
-    #   Program.create_from_paths(['/bin/cd','/usr/bin/cd']) # => Program
+    #   Program.find_with_paths(['/bin/cd','/usr/bin/cd']) # => Program
     #
-    #   Program.create_from_paths(['/obviously/fake','/bla']) # => nil
+    #   Program.find_with_paths(['/obviously/fake','/bla']) # => nil
     #
-    def self.create_from_paths(paths,*args,&block)
+    def self.find_with_paths(paths,*args,&block)
       paths.each do |path|
         return self.new(path,*args,&block) if File.file?(path)
       end
@@ -70,13 +70,13 @@ module RProgram
     # +program_names+, a ProramNotFound exception will be raised. Any given
     # _args_ or a given _block_ will be used in creating the new program.
     #
-    #   Program.create # => Program
+    #   Program.find # => Program
     #
-    #   MyProgram.create('stuff','here') do |prog|
+    #   MyProgram.find('stuff','here') do |prog|
     #     ...
     #   end
     #
-    def self.create(*args,&block)
+    def self.find(*args,&block)
       path = Compat.find_program_by_names(program_names)
       unless path
         names = program_names.map { |name| name.dump }.join(', ')
