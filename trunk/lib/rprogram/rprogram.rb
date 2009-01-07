a92ae527b490cbda4e4ff6ea6e16c9127a831f00
Postmodern <postmodern.mod3@gmail.com>
2009-01-07 23:33:48 +0000

    Added documentation.

--- trunk/lib/rprogram/rprogram.rb
+++ trunk/lib/rprogram/rprogram.rb
@@ -1,8 +1,16 @@
 module RProgram
+  #
+  # Returns +true+ if RProgram debugging messages are enabled, returns
+  # +false+ if they are not enabled.
+  #
   def RProgram.debug
     @@debug ||= false
   end
 
+  #
+  # Enables or disables RProgram debugging messages depending on the
+  # specified _value_.
+  #
   def RProgram.debug=(value)
     @@debug = value
   end
