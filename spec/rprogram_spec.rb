8807891e7df8d19fabdc94a39a3e752c603106e9
postmodern <postmodern.mod3@gmail.com>
2009-06-10 18:14:31 -0700

    Specs.

--- spec/rprogram_spec.rb
+++ spec/rprogram_spec.rb
@@ -6,4 +6,12 @@ describe RProgram do
   it "should have a VERSION constant" do
     RProgram.const_defined?('VERSION').should == true
   end
+
+  it "should have a debug mode" do
+    RProgram.debug = true
+    RProgram.debug.should == true
+
+    RProgram.debug = false
+    RProgram.debug.should == false
+  end
 end
