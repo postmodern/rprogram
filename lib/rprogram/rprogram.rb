4b21166f25c36207f18133042e344cf85fbd711d
postmodern <postmodern.mod3@gmail.com>
2009-09-03 17:00:24 -0700

    Added YARD tags to top-level RProgram methods.

--- lib/rprogram/rprogram.rb
+++ lib/rprogram/rprogram.rb
@@ -1,15 +1,17 @@
 module RProgram
   #
-  # Returns +true+ if RProgram debugging messages are enabled, returns
-  # +false+ if they are not enabled.
+  # @return [true, false] Specifies whether debugging messages are enabled
+  #                       for RProgram. Defaults to false, if not set.
   #
   def RProgram.debug
     @@rprogram_debug ||= false
   end
 
   #
-  # Enables or disables RProgram debugging messages depending on the
-  # specified _value_.
+  # Enables or disables debugging messages for RProgram.
+  #
+  # @param [true, false] value The new value of RProgram.debug.
+  # @return [true, false] The new value of RProgram.debug.
   #
   def RProgram.debug=(value)
     @@rprogram_debug = value
