5ce88edc50974d70a7383879064273b2a1ca5277
Postmodern <postmodern.mod3@gmail.com>
2011-04-01 16:51:56 -0700

    Added specs for Compat.run.

--- spec/compat_spec.rb
+++ spec/compat_spec.rb
@@ -1,6 +1,7 @@
 require 'rprogram/compat'
 
 require 'spec_helper'
+require 'tempfile'
 
 describe Compat do
   it "should have a list of directories that contain programs" do
@@ -17,5 +18,26 @@ describe Compat do
 
   it "should be able to find programs by multiple names" do
     File.executable?(Compat.find_program_by_names('ls','dir')).should == true
+  end
+
+  describe "run" do
+    subject { Compat }
+    let(:dir) { Compat.find_program('dir') }
+
+    it "should return true when programs succeed" do
+      subject.run(dir).should == true
+    end
+
+    it "should return false when programs fail" do
+      subject.run(dir,'-zzzzzz').should == false
+    end
+
+    it "should allow passing spawn options as the last argument" do
+      output = Tempfile.new('rprogram_compat_run')
+
+      subject.run(dir,'-l',:out => [output.path, 'w'])
+
+      output.read.should_not be_empty
+    end
   end
 end
